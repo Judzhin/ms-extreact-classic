318b03db8c62b95bb103928a5529dc1e865b5bcb
Judzhin Miles <info@msbios.com>
2018-05-21 18:44:33 +0300

    Intermedaite commit

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = function (env) {
             new ExtJSReactorWebpackPlugin({
                 sdk: 'ext', // you need to copy the Ext JS SDK to the root of this package, or you can specify a full path to some other location
                 toolkit: 'classic',
-                theme: './ext-react/packages/custom-ext-react-theme',
+                theme: './ext-react/packages/ms-theme-dashboard',
                 overrides: ['overrides'],
                 packages: [
                     'font-ext',
